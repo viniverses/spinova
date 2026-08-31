c47b0ce26d1593c42864239e6508f78a8187a734
Vinícius Silva <vinisuxx@outlook.com>
2026-08-31 12:23:04 -0300

    chore(api): add pnpm lockfile and update dependencies for environment configuration

--- apps/api/src/app.ts
+++ apps/api/src/app.ts
@@ -2,17 +2,25 @@ import { node } from "@elysia/node";
 import { Elysia } from "elysia";
 
 import { corsPlugin } from "@/plugins/cors.ts";
+import { env } from "@package/env/server";
 import { auth } from "@repo/auth";
+import { hostname } from "os";
 
 const app = new Elysia({ adapter: node() });
 
 app.use(corsPlugin);
 app.mount(auth.handler);
 
-app.listen(process.env.PORT, ({ hostname, port }) => {
-  console.log(
-    `[💿 Spinova API] v${process.env.API_VERSION} is running at ${hostname}:${port}`,
-  );
-});
+app.listen(
+  {
+    port: env.PORT,
+    hostname: env.API_HOSTNAME,
+  },
+  ({ hostname: host, port }) => {
+    console.log(
+      `[💿 Spinova API] v${env.API_VERSION} is running at ${host}:${port}`,
+    );
+  },
+);
 
 export { app };
